9ebd0323c2ab99e18819844987fa39abc3b6fa62
Ben Tyler <benjamin.tyler@gmail.com>
2016-04-24 20:09:43 +0200

    process name alignment

--- lib/ricor_ex/ricor.ex
+++ lib/ricor_ex/ricor.ex
@@ -3,9 +3,9 @@ defmodule RicorEx.Service do
   Record.extract_all(from_lib: "riak_core/include/riak_core_vnode.hrl")
   def ping do
     doc_idx = :riak_core_util.chash_key({"ping", :erlang.term_to_binary(:os.timestamp())})
-    pref_list = :riak_core_apl.get_primary_apl(doc_idx, 1, :ricor)
+    pref_list = :riak_core_apl.get_primary_apl(doc_idx, 1, RicorEx.Service)
     [{index_node, _type}] = pref_list
-    # RicorEx.VnodeMaster is the process name from RicorEx.Sup
-    :riak_core_vnode_master.sync_spawn_command(index_node, :ping, RicorEx.VnodeMaster)
+    # riak core appends "_master" to RicorEx.Vnode.
+    :riak_core_vnode_master.sync_spawn_command(index_node, :ping, RicorEx.Vnode_master)
   end
 end
--- lib/ricor_ex/sup.ex
+++ lib/ricor_ex/sup.ex
@@ -2,11 +2,12 @@ defmodule RicorEx.Sup do
   use Supervisor
 
   def start_link do
-    Supervisor.start_link({:local, __MODULE__}, __MODULE__, [])
+    Supervisor.start_link(__MODULE__, [name: __MODULE__])
   end
 
   def init(_args) do
-    vmaster = { RicorEx.VnodeMaster, 
+    # riak_core appends _master to 'RicorEx.Vnode'
+    vmaster = { RicorEx.Vnode_master,
       { :riak_core_vnode_master, :start_link, [RicorEx.Vnode] },
       :permanent, 5000, :worker, [:riak_core_vnode_master] }
 
