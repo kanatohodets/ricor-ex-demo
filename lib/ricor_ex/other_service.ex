0a95d4b75e89ad0b1eb3b181ef5a0ffc022f67a1
Ben Tyler <benjamin.tyler@gmail.com>
2016-05-05 22:50:58 +0200

    n -> 1, other code expects it

--- lib/ricor_ex/other_service.ex
+++ lib/ricor_ex/other_service.ex
@@ -4,7 +4,7 @@ defmodule RicorEx.OtherService do
   Record.extract_all(from_lib: "riak_core/include/riak_core_vnode.hrl")
   def blorg do
     doc_idx = :riak_core_util.chash_key({"blorg", :erlang.term_to_binary(:os.timestamp())})
-    pref_list = :riak_core_apl.get_primary_apl(doc_idx, 5, RicorEx.OtherService)
+    pref_list = :riak_core_apl.get_primary_apl(doc_idx, 1, RicorEx.OtherService)
     Logger.warn("pref list: #{inspect(pref_list)}")
     [{index_node, _type}] = pref_list
     # riak core appends "_master" to RicorEx.OtherVnode.
