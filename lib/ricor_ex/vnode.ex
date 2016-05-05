3eec028e7ec9004e3202b350bf690f94908921a3
Ben Tyler <benjamin.tyler@gmail.com>
2016-05-05 22:46:22 +0200

    data handoff example

--- lib/ricor_ex/vnode.ex
+++ lib/ricor_ex/vnode.ex
@@ -3,14 +3,18 @@ defmodule RicorEx.Vnode do
   require Logger
   require Record
 
-  Record.defrecord :state, [:partition]
+  Record.defrecord :riak_core_fold_req_v2, Record.extract(:riak_core_fold_req_v2, from_lib: "riak_core/include/riak_core_vnode.hrl")
+  Record.defrecord :state, [:partition, :table]
 
   def start_vnode(partition) do
     :riak_core_vnode_master.get_vnode_pid(partition, __MODULE__)
   end
 
   def init([partition]) do
-    {:ok, state(partition: partition)}
+    ets_handle = :ets.new(nil, [])
+    :ets.insert(ets_handle, {:foo, [1, 2, 3]})
+    :ets.insert(ets_handle, {:bar, [4, 5, 6]})
+    {:ok, state(partition: partition, table: ets_handle)}
   end
 
   def handle_command(:ping, _sender, state) do
@@ -28,9 +32,15 @@ defmodule RicorEx.Vnode do
     {:reply, {:pong, state(state, :partition)}, state}
   end
 
-  def handle_handoff_command(message, _sender, state) do
-    Logger.warn("unhandled handoff command: #{inspect(message)}")
-    {:noreply, state}
+  def handle_handoff_command(riak_core_fold_req_v2(foldfun: visit_fun, acc0: acc0)=_fold_req, _sender, state(table: ets_handle)=state) do
+    fold_fn = fn(object, acc_in) ->
+      Logger.warn("folding over thing: #{inspect object}")
+      acc_out = visit_fun.({:foo_bucket, object}, object, acc_in)
+      Logger.warn("what is my acc out? #{inspect acc_out}")
+      acc_out
+    end
+    final = :ets.foldl(fold_fn, acc0, ets_handle)
+    {:reply, final, state}
   end
 
   def handoff_starting(_target_node, state) do
@@ -49,18 +59,19 @@ defmodule RicorEx.Vnode do
   end
 
   def handle_handoff_data(data, state) do
-    Logger.warn("got some handoff data #{inspect([data, state])}")
+    {key, value} = :erlang.binary_to_term(data)
+    Logger.warn("got some handoff data: key: #{ inspect key } and value: #{ inspect value }")
     {:reply, :ok, state}
   end
 
   def encode_handoff_item(object_name, object_value) do
     Logger.warn("encoding a handoff item#{inspect([object_name, object_value])}")
-    ""
+    :erlang.term_to_binary({object_name, object_value})
   end
 
-  def is_empty(state) do
+  def is_empty(state(table: ets_handle)=state) do
     Logger.warn("checking for emptiness")
-    {true, state}
+    {:"$end_of_table" === :ets.first(ets_handle), state}
   end
 
   def delete(state) do
